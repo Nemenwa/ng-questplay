086521399153cf7ba4f5634a9983f861d8719f66
Ebele Lynda Okolo <138141666+Nemenwa@users.noreply.github.com>
2026-04-02 04:26:02 +0100

    Update Challenge.sol

--- campaigns/gas-optimization/solidity-optimization/contracts/Challenge.sol
+++ campaigns/gas-optimization/solidity-optimization/contracts/Challenge.sol
@@ -2,42 +2,10 @@
 pragma solidity ^0.8.19;
 
 contract Challenge {
-    uint256 immutable _SKIP;
+    uint256 private immutable SKIP_VALUE;
+
     error Overflow();
 
-    constructor(uint256 skip) {
-        _SKIP = skip;
+    constructor(uint256 skip_) {
+        SKIP_VALUE = skip_;
     }
-
-    // /**
-    //  * @notice Returns  the sum of the elements of the given array, skipping any SKIP value.
-    //  * @param array The array to sum.
-    //  * @return The sum of all the elements of the array excluding SKIP.
-    //  */
-    function sumAllExceptSkip(
-        uint256[] calldata array
-    ) public view returns (uint256 sum) {
-        uint256 skip = _SKIP;
-
-        assembly {
-            let offset := array.offset
-            let end := add(offset, mul(array.length, 0x20))
-
-            // Loop through the array in calldata
-            for { let i := offset } lt(i, end) { i := add(i, 0x20) } {
-                let val := calldataload(i)
-
-                // Only add if not equal to skip value
-                if iszero(eq(val, skip)) {
-                    // Check for overflow before adding
-                    let newSum := add(sum, val)
-                    if lt(newSum, sum) {
-                        mstore(0x00, 0x35278d12) // Custom error code for overflow
-                        revert(0x00, 0x04)
-                    }
-                    sum := newSum
-                }
-            }
-        }
-    }
-}
